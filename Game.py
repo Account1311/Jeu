4c6ae8154f1d323f69887113a0a4eeefb555cefd
Pauline BEHAR <p.behar@saint-louis29.net>
2025-03-12 11:33:25 +0100

    ajout des ennemis

--- Game.py
+++ Game.py
@@ -1,5 +1,5 @@
 import pyxel
-
+import random
 
 ecran_l = 128
 ecran_h = 128
@@ -18,6 +18,10 @@ vaisseau_h = 8
 
 # initialisation des tirs
 tirs_liste = []
+
+ennemis_liste = []
+
+
 
 def vaisseau_deplacement(x, y):
     """déplacement avec les touches de directions"""
@@ -44,6 +48,41 @@ def tirs_creation(x, y, tirs_liste):
     return tirs_liste
 
 
+def tirs_deplacement(tirs_liste):
+    """déplacement des tirs vers le haut et suppression s'ils sortent du cadre"""
+    for tir in tirs_liste:
+        tir[1] -= 1 # le tir se déplace vers le haut
+        if  tir[1]< 0 : #le tir sort de l'écran
+            tirs_liste.remove(tir)# je supprime ce tir de la liste des tirs
+    return tirs_liste
+
+
+def ennemis_creation(ennemis_liste):
+
+    if pyxel.frame_count % 30 == 0:
+        ennemis_liste.append([random.randrange(0, 128), 0])
+    return ennemis_liste
+
+def ennemis_maj(ennemis_liste):
+    for ennemi in ennemis_liste:
+        ennemi[1] += 1
+        if ennemi[1] > 128 or ennemi[1] < 0:
+            ennemis_liste.remove(ennemi)
+    return ennemis_liste
+
+
+def ennemis_suppression():
+    """disparition d'un ennemi et d'un tir si contact"""
+    to_remove = []  # Liste pour stocker les ennemis à supprimer
+    for ennemi in ennemis_liste:
+        e_x, e_y = ennemi
+        for tir in tirs_liste:
+            tir_x, tir_y = tir
+            if e_x < tir_x + 4 and e_x + 8 >= tir_x and e_y < tir_y + 6 and e_y + 7 >= tir_y:
+                to_remove.append(ennemi)  # Ajoute l'ennemi à supprimer
+                tirs_liste.remove(tir) # suppression de la liste
+    for ennemi in to_remove:
+        ennemis_liste.remove(ennemi)  # Suppression des ennemis après la boucle
 
 
 
@@ -55,16 +94,28 @@ pyxel.load("images.pyxres")
 def update():
     """mise à jour des variables (30 fois par seconde)"""
 
-    global vaisseau_x, vaisseau_y, vaisseau_l, vaisseau_h, tirs_liste
+    global vaisseau_x, vaisseau_y, vaisseau_l, vaisseau_h, tirs_liste, ennemis_liste
     
     vaisseau_x, vaisseau_y = vaisseau_deplacement(vaisseau_x, vaisseau_y)
 
     #print(pyxel.frame_count)
     
      # creation des tirs en fonction de la position du vaisseau
-    tirs_liste = tirs_creation(vaisseau_x + 4, vaisseau_y, tirs_liste)
+    tirs_liste = tirs_creation(vaisseau_x + 2, vaisseau_y - 2, tirs_liste)
 
- 
+     # mise a jour des positions des tirs
+    tirs_liste = tirs_deplacement(tirs_liste)
+    
+    
+    ennemis_liste = ennemis_creation(ennemis_liste)
+
+
+    ennemis_liste = ennemis_maj(ennemis_liste)
+
+    #suppression des ennemis si contact avec tir
+    ennemis_suppression()
+
+
 # =========================================================
 # == DRAW
 # =========================================================
@@ -76,7 +127,7 @@ def draw():
     
 # tirs
     for tir in tirs_liste: # je boucle sur ma liste de tirs
-        pyxel.rect(tir[0], tir[1], 1, 4, 10) #je dessine un rectangle
+        pyxel.blt(tir[0], tir[1], 0, 8, 8, 4, 6)
 
     # vaisseau (carre 8x8)
     # x, y, largeur, hauteur, couleur
@@ -84,6 +135,11 @@ def draw():
         pyxel.blt(vaisseau_x, vaisseau_y, 0, 0, 0, vaisseau_l, vaisseau_h)
     else:
         pyxel.blt(vaisseau_x, vaisseau_y, 0, 8, 0, vaisseau_l, vaisseau_h)
+
+ # ennemis
+    for ennemi in ennemis_liste: # je boucle sur ma liste d’ennemis
+        pyxel.blt(ennemi[0], ennemi[1], 0, 0, 9, 8, 7)
+
 
 # lance le programme principal
 pyxel.run(update, draw)
